29175a56e6525fa07bb2f5d63978def642cb180c
yuanli <601624481@qq.com>
2022-01-10 17:02:53 +0800

    chunk

---
+++ lodash/yuan4li-lodash.js
@@ -0,0 +1,21 @@
+var yuan4li = {
+  chunk: (array, size) => {
+    let n = array.length
+    if (n <= size) { return [array] }
+    let temp = []
+    let result = []
+
+    for (i = 0; i < n; i++){
+      temp.push = array[i]
+      if (temp.length == size) {
+        result.push(temp)
+        temp = []
+      }
+    }
+    if (temp.length != 0) {
+      result.push(temp)
+    }
+    return result
+  }
+}
+ 
